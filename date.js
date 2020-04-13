22ee57487fef847cc3c9739dbed1e1405b4b6b6c
ntwntw369 <60605154+ntwntw369@users.noreply.github.com>
2020-04-13 18:02:13 +0900

    퀴즈추가
    붕붕때라때라방샤

--- date.js
+++ date.js
@@ -1,4 +1,1 @@
-var today = new Date(); //컴퓨터의 로컬기준 today 에 Date 객체 삽입 , 오늘날짜
-var date = new Date(); 
 
-document.write(4 * 5);
--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+
 const myHeading = document.querySelector('h2');
 myHeading.textContent = 'Hi, World!';
+
+
+
 
