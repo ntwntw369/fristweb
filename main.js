2d4dda46ab2ae08a0ebdd4d7b2dea78ebfd46da6
ntwntw369 <60605154+ntwntw369@users.noreply.github.com>
2020-08-02 17:14:21 +0900

    커서사진추가
    이히히 커서이미지 넣었씀
    
    he- he- I put the cursor
    
    html{
    	cursor: url(pointer.cur), auto
            ##         앞에부터 우선순위로 적용 안되면 다음꺼
    }

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
+function go(){
+	var input;
+	input = prompt('소원을 말해봐', '내 소원은..');
+	document.write("<h1>너의 소원 '"+input+"'은 잘 받았다.</h>");
+	setTimeout(function() {
+	location.href=location.href;
+	}, 3000);
 
+}
+
+	
 const myHeading = document.querySelector('h2');
 myHeading.textContent = 'Hi, World!';
+
+
+
 
 
 
